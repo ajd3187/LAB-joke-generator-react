21ef71da0144de08f52193c3bafcfb86a834e19a
Aaron Dugas <ajd3187@gmail.com>
2024-01-09 19:48:48 -0600

    basic blocks of react Joke

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,15 @@
+import { useState } from 'react';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [joke, newJoke] = useState('');
+
+  const handleClicked = () => {
+    getJoke().then((newJokeValue) => {
+      newJoke(newJokeValue);
+    });
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -10,6 +21,8 @@ function Home() {
       }}
     >
       <h1>Welcome Home!</h1>
+      <h2>{joke}</h2>
+      <button type="button" onClick={handleClicked}>Get a Joke!</button>
     </div>
   );
 }
